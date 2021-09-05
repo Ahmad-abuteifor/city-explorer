cc31bf2d7cc9c96373eabaa49106caab3ef16189
Ahmad-abuteifor <ana1ahmad10@gmail.com>
2021-09-05 15:46:45 +0300

    lab06 make the form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import axios from 'axios';
+
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+constructor(props){
+super(props)
+this.state={
+nameOflocation:''
+}
+}
+
+forChange=(e)=>{
+this.setState({
+  nameOflocation:e.target.value
+})
+
+// console.log(e.target.value);
+}
+
+
+forSubmit= async (e)=>{
+  e.preventDefault();
+
+  console.log(this.state.nameOflocation);
+
+  const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.nameOflocation}&format=json`;
+  console.log(url);
+
+  const theResponse = await axios.get(url);
+console.log(theResponse.data[0]);
+
+this.setState({
+  nameOflocation:theResponse.data[0]
+})
+  
+
+  
+}
+
+
+
+
+
+
+render(){
+
+  return(
+<main>
+<Form onSubmit={this.forSubmit}>
+  <Form.Group className="mb-3" controlId="formBasicEmail"   >
+    <Form.Label> location name </Form.Label>
+    <Form.Control type="text" placeholder="address"  onChange={this.forChange} />
+    <Form.Text className="text-muted">
+      enter the name of the city that woud like to see where its location 
+    </Form.Text>
+  </Form.Group>
+
+ 
+ 
+  <Button variant="primary" type="submit"  >
+    Submit
+  </Button>
+</Form>
+
+
+  <h2>Location Info </h2>
+  <p>{this.state.nameOflocation.display_name}</p>
+  <p>{this.state.nameOflocation.lat}</p>
+  <p>{this.state.nameOflocation.lon}</p>
+
+
+
+
+
+
+</main>
+
+  )
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
 }
 
 export default App;

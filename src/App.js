ab6131cdc91568d4e8118768251cb23e58dd40b5
Ahmad-abuteifor <ana1ahmad10@gmail.com>
2021-09-07 16:33:58 +0300

    some change to labe06and 7

--- src/App.js
+++ src/App.js
@@ -1,107 +1,97 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Formlocation from './components/form';
 import axios from 'axios';
 
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component{
-constructor(props){
-super(props)
-this.state={
-nameOflocation:'',
-mapLocation:''
-}
-}
-
-forChange=(e)=>{
-this.setState({
-  nameOflocation:e.target.value
-})
-
-// console.log(e.target.value);
-}
-
-
-forSubmit= async (e)=>{
-  e.preventDefault();
-
-  console.log(this.state.nameOflocation);
-
-  const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.nameOflocation}&format=json`;
-  console.log(url);
-
- 
-  const theResponse = await axios.get(url);
-console.log(theResponse.data[0]);
-
-this.setState({
-  nameOflocation:theResponse.data[0]
-})
-  
-// const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${this.state.nameOflocation.lat},${this.state.nameOflocation.lon}&zoom=1-18`
-
-// const mapResponse= await axios.get(mapUrl)
-// console.log(mapResponse);
-// this.setState({
-//   mapLocation:mapResponse
-// })
-  
-}
-
-
-
-
-
-
-
-
-render(){
-
-
-  const pStyle={
-
-    color: "black",
-    margin:"20px",
-    fontFamily: "Arial"
-  
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nameOflocation: '',
+      // mapLocation: '',
+      locationdetails: false,
+      wetherDeatales:[]
+    }
   }
+
+  forChange = (e) => {
+    this.setState({
+      nameOflocation: e.target.value
+    })
+
+    console.log(e.target.value);
+  }
+
+
+  forSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(this.state.nameOflocation);
+
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.nameOflocation}&format=json`;
+    console.log(url);
+
+
+    const serverUrl=`${process.env.REACT_APP_SERVER_URL}/weathers?&city_name=${this.state.nameOflocation}`
+
+    const theResponse = await axios.get(url);
+
+    const serverResponse=await axios.get(serverUrl);
+    console.log(theResponse.data[0]);
+
+    this.setState({
+      nameOflocation: theResponse.data[0],
+      locationdetails: true,
+      wetherDeatales: serverResponse
+    })
+
+   
+  }
+
+
+
+
+
+
+
+
+  render() {
+
+
+    const pStyle = {
+
+      color: "black",
+      margin: "20px",
+      fontFamily: "Arial"
+
+    }
+
+    let weatherstuff=this.state.wetherDeatales.map(item=>{
+      return <p>{item.date}:{item.description}</p>
+    })
   
 
-  return(
-<main>
-<Form  style={pStyle}  onSubmit={this.forSubmit}>
-  <Form.Group className="mb-3" controlId="formBasicEmail"   >
-    <Form.Label> location name </Form.Label>
-    <Form.Control type="text" placeholder="address"  onChange={this.forChange} />
-    <Form.Text className="text-muted">
-      enter the name of the city that woud like to see where its location 
-    </Form.Text>
-  </Form.Group>
+    return (
+      <main>
+       <Formlocation
+       forSubmit={this.forSubmit}
+       forChange={this.forChange}
 
- 
- 
-  <Button variant="primary" type="submit" style={{color: "black"} ,{backgroundColor:"blue"}} >
-    Submit
-  </Button>
-</Form>
+       
+       />
 
-
-  <h2 style={{color: "red"}}>Location Info </h2>
-  <p  style={pStyle} >{this.state.nameOflocation.display_name}</p>
-  <p style={pStyle}>{this.state.nameOflocation.lat}</p>
-  <p style={pStyle}>{this.state.nameOflocation.lon}</p>
+        {
+          this.state.locationdetails &&
+          <div>
+    <h2 style={{color: "red"}}>Location Info </h2>
+  <p  style={pStyle} > location {this.state.nameOflocation.display_name}</p>
+  <p style={pStyle}>lat :{this.state.nameOflocation.lat}</p>
+  <p style={pStyle}>lon :{this.state.nameOflocation.lon}</p>
   <img  style={pStyle}src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&center=${this.state.nameOflocation.lat},${this.state.nameOflocation.lon}&zoom=1-18`} alt=""/>
 
-
-
-
-
-
-</main>
-
-  )
+  <p>{weatherstuff}</p>
 
 
 
@@ -109,7 +99,27 @@ render(){
 
 
 
-}
+
+
+
+  </div>
+
+
+        }
+
+
+
+      </main>
+
+    )
+
+
+
+
+
+
+
+  }
 
 
 
